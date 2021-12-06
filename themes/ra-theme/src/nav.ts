2b06bce99282139a4a3d9a588b1111ca50238ed4
Mike Koss <mike@mckoss.com>
2021-12-06 01:55:51 -0800

    Refactor scss files.

--- themes/ra-theme/src/nav.ts
+++ themes/ra-theme/src/nav.ts
@@ -5,7 +5,6 @@ function main() {
   const toggle = document.querySelector('#menu-toggle')!;
 
   toggle.addEventListener('click', () => {
-    console.log("Toggle menu");
     const pop = nav.getAttribute('data-popout');
     nav.setAttribute('data-popout', pop === 'true' ? 'false' : 'true');
     toggle.setAttribute('data-popout', pop === 'true' ? 'false' : 'true');
