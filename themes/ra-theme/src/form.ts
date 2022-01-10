f351faa0994abaf6c1c2a4d9692547d5b9b51173
Mike Koss <mike@mckoss.com>
2022-01-09 20:14:44 -0800

    Process other fields.

--- themes/ra-theme/src/form.ts
+++ themes/ra-theme/src/form.ts
@@ -16,22 +16,23 @@ register('form', (options: any) => {
   const elements = form.querySelectorAll('input, select, textarea');
   for (const elt of elements) {
       const name = elt.getAttribute('name');
+
       if (name === null) {
         continue;
       }
-      if (name.endsWith('-other')) {
-        elt.setAttribute('disabled', 'true');
+
+      if (elt.getAttribute('value') === 'other') {
+        handleOtherControl(elt as HTMLInputElement);
       }
+
       if (name && !names.includes(name)) {
         names.push(name);
         controls.set(name, elt as HTMLElement);
-        let listElt = elt.parentElement!;
-        while (listElt.tagName != 'LI') {
-          listElt = listElt.parentElement!;
+        let listElt = parentListElement(elt);
+        if (listElt) {
+          listElements.set(name, listElt);
         }
-        listElements.set(name, listElt);
       }
-      console.log(name);
   }
 
   form.onsubmit = submitForm;
@@ -70,15 +71,22 @@ register('form', (options: any) => {
       }
     }
 
+    for (let name of names) {
+      const value = data.get(name);
+      if (value === null || value == '') {
+        continue;
+      }
+      if (name.endsWith('-other')) {
+        name = name.slice(0, -6);
+      }
+      json[name] = value;
+    }
+
+    console.log(json);
+
     if (firstError) {
       return false;
     }
-
-    for (const [key, value] of data.entries()) {
-      json[key] = value;
-      console.log(`${key}: type {${typeof value}} value {${value}}`);
-    }
-    console.log(json);
   }
 });
 
@@ -87,4 +95,49 @@ function makeError(): HTMLElement {
   required.classList.add('form-error');
   required.textContent = 'This field is required.';
   return required;
+}
+
+// Find the parent list element of a form element.
+function parentListElement(elt: Element): Element | null {
+  let listElt = elt.parentElement!;
+  while (listElt !== null && listElt.tagName !== 'LI') {
+    listElt = listElt.parentElement!;
+  }
+  return listElt;
+}
+
+// Other type radio button.  There is a linked input field that should
+// be disabled or enabled based on the state of the radio button.
+// Note that we need to use change event on a parent element, since
+// deselecting the Other button by clicking a sibling sends no event to itself.
+function handleOtherControl(other: HTMLInputElement) {
+  const name = other.getAttribute('name')!;
+  let li = parentListElement(other)!;
+  const otherText = li.querySelector(`[name="${name}-other"]`) as HTMLInputElement;
+  otherText.setAttribute('disabled', 'true');
+
+  if (!otherText) {
+    console.log(`No linked text input for other ${name}.`);
+    return;
+  }
+
+  otherText.addEventListener('click', (e: Event) => {
+    console.log(`Clicked other ${name}.`);
+    other.checked = true;
+  });
+
+  li.addEventListener('change', (evt) => {
+    const radio = evt.target as HTMLInputElement;
+    if (radio.tagName !== 'INPUT' || radio.type !== 'radio') {
+      return;
+    }
+
+    if (radio.value === 'other') {
+      otherText.disabled = false;
+      otherText.focus();
+    } else {
+      otherText.disabled = true;
+      otherText.value = '';
+    }
+  });
 }
